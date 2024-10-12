dd2d2d3dec1fdfda80a26ae316eaf14e846dee0d
HabrielStark <gabrielldubov12@gmail.com>
2024-10-12 12:55:39 +0200

    Update textarea.tsx

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,24 +1,24 @@
-import * as React from "react"
+import * as React from &quot;react&quot;
 
-import { cn } from "@/lib/utils"
+import { cn } from &quot;@/lib/utils&quot;
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes&lt;HTMLTextAreaElement&gt; {}
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
+const Textarea = React.forwardRef&lt;HTMLTextAreaElement, TextareaProps&gt;(
+  ({ className, ...props }, ref) =&gt; {
     return (
-      <textarea
+      &lt;textarea
         className={cn(
-          "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          &quot;flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50&quot;,
           className
         )}
         ref={ref}
         {...props}
-      />
+      /&gt;
     )
   }
 )
-Textarea.displayName = "Textarea"
+Textarea.displayName = &quot;Textarea&quot;
 
 export { Textarea }
